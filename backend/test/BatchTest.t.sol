5fe94d97453e7bc0dab074bc585189d42d4f3e88
ayodeji <ayodejiakintobi1@gmail.com>
2025-03-21 22:09:36 +0100

    CREATE: TEST FILE

--- backend/test/BatchTest.t.sol
+++ backend/test/BatchTest.t.sol
@@ -1,41 +1,20 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
-// import {Test, console} from "forge-std/Test.sol";
-// import {BatchPay} from "../src/BatchPay.sol";
 
-// contract BatchTest is Test {
-//     Batch batch;
+import {BatchPay} from  "../BatchPay";
+import {Test} from "forge-std/Test.sol";
 
-//     uint256 oneEther = 1e18;
 
-//     function setUp() external {
-//         batch = new Batch();
-//     }
+contract BatchTest is Test {
+    BatchPay batchPay;
+    address owner = address(this);
 
-//     //Test if owner is actully the real owner
-//     function testOwnerIsDeployer() external {
-//         assertEq(batch.owner(), address(this));
-//     }
+    address employee1 = address(0x123);
+    address employee2 = address(0x234);
 
-//     //Test if owner can add employee
-//     function testOwnerCanAddEmployee() external {
-//         batch.addEmployee(address(0x123), oneEther);
-//         assertEq(batch.getEmployeesSalaries(address(0x123)), oneEther);
-//     }
-
-//    //Test addEmployee then remove then confirm zero index address balance
-//     function testOwnerCanDeleteEmployee() external {
-//         batch.addEmployee(address(0x123), oneEther);
-//         batch.removeEmployee(address(0x123));
-//         assertEq(batch.getEmployeesSalaries(address(0x123)), 0);
-//     }
-
-//     //Test actuall payment by owner and recieving end
-//     function testOwnerCanMakePayment() public {
-//         batch.addEmployee(address(0x123), oneEther);
-//         batch.depositFunds{ value: 1 ether }();
-
-//         assertEq(address(batch).balance, 1 ether); //confirm  balance
-//     }
-// }
+    function setUp() {
+        batchPay = new BatchPay(owner);`
+    }
+    function testAddEmployee()
+}
