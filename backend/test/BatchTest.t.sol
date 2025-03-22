763f7ac8167cc592ae53b7a96a0431fdeab300da
ayodeji <ayodejiakintobi1@gmail.com>
2025-03-22 17:39:04 +0100

    UPDATE: TEST FUNCTIONS ADDED

--- backend/test/BatchTest.t.sol
+++ backend/test/BatchTest.t.sol
@@ -1,10 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
-
-import {BatchPay} from  "../BatchPay";
+import {BatchPay} from "../src/BatchPay.sol";
 import {Test} from "forge-std/Test.sol";
-
 
 contract BatchTest is Test {
     BatchPay batchPay;
@@ -13,8 +11,19 @@ contract BatchTest is Test {
     address employee1 = address(0x123);
     address employee2 = address(0x234);
 
-    function setUp() {
-        batchPay = new BatchPay(owner);`
+    function setUp() public {
+        batchPay = new BatchPay(owner);
     }
-    function testAddEmployee()
+    function testIfOwnerIsOwner() public view {
+        assertEq(batchPay.owner(), owner, "Owner should be deploof contract");
+    }
+
+    function testAddEmployee() public  {
+        batchPay.addEmployee(employee1, 1 ether);
+        assertEq(
+            batchPay.getEmployeesSalaries(employee1),
+            1 ether,
+            "Employee's address should be 1 ether"
+        );
+    }
 }
