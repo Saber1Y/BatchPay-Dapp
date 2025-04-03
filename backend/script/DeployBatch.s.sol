ff83356df7a98ca24000d48842268fb6b37350a2
ayodeji <ayodejiakintobi1@gmail.com>
2025-04-03 13:21:33 +0100

    UPDATE:  Owner initialised

--- backend/script/DeployBatch.s.sol
+++ backend/script/DeployBatch.s.sol
@@ -1,14 +1,19 @@
-// // SPDX-License-Identifier: MIT
-// pragma solidity 0.8.20;
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.20;
 
-// import {Script} from "forge-std/Script.sol";
-// import {BatchPay} from "../src/BatchPay.sol";
+import {Script} from "forge-std/Script.sol";
+import {BatchPay} from "../src/BatchPay.sol";
 
-// contract DeployBatch is Script {
-//     function run() external returns (Batch) {
-//         vm.startBroadcast();
-//         Batch batch = new Batch();
-//         vm.stopBroadcast();
-//         return batch;
-//     }
-// }
+contract DeployBatch is Script {
+    function run() external returns (BatchPay) {
+
+        vm.startBroadcast(); // Starts sending transactions from deployer wallet
+        
+        address owner = msg.sender;
+        BatchPay batch = new BatchPay(owner); // Deploys the BatchPay contract
+
+        vm.stopBroadcast(); // Stops broadcasting transactions
+
+        return batch; // Returns the deployed contract instance
+    }
+}
